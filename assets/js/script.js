8dbd59a1599e7608f1a8545ea866e561061a7d91
Oscar-UCT <ocariaga2024@alu.uct.cl>
2025-04-22 15:02:40 -0400

    New new new contact form

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,24 @@ navbarBtn.addEventListener("click", ()=>
         navbar.style.opacity = "0";
     }
 })
+
+function CerrarVentanaCorreo()
+{
+    document.getElementById("ventana-correo").style.display = "none"
+}
+function MostrarVentanaCorreo()
+{
+    document.getElementById("ventana-correo").style.display = "flex"
+}
+
+const form = document.getElementById('formulario-contacto');
+
+form.addEventListener('submit', function(event) {
+    // Función "mock" de lo que sería una función para enviar correo
+    if (!form.checkValidity()) { // Revisa si los campos están, si no es así, no continua la función.
+        return;
+    }
+    event.preventDefault();
+    alert('Correo enviado');
+    window.location.href = 'index.html';
+});
