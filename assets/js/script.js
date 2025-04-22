bacc9bba86c7b23f59b7214d94595c3abaa0a86e
Oscar-UCT <ocariaga2024@alu.uct.cl>
2025-04-22 15:52:27 -0400

    Comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Navbar que aparece en cierto tamaño de pantalla
 const navbar = document.getElementById("navbar-appear")
 const navbarBtn = document.getElementById("navbar-btn")
 navbarBtn.addEventListener("click", ()=>
@@ -14,6 +15,7 @@ navbarBtn.addEventListener("click", ()=>
     }
 })
 
+// Funciones para controlar la visualización de la ventana de enviar mensaje
 function CerrarVentanaCorreo()
 {
     document.getElementById("ventana-correo").style.display = "none"
@@ -23,16 +25,17 @@ function MostrarVentanaCorreo()
     document.getElementById("ventana-correo").style.display = "flex"
 }
 
-const form = document.getElementById('formulario-contacto');
 
+const form = document.getElementById('formulario-contacto');
 form.addEventListener('submit', function(event) {
-    // Función "mock" de lo que sería una función para enviar correo
+    // Función "mock" de lo que sería una función para enviar un mensaje
     if (!form.checkValidity()) { // Revisa si los campos están, si no es así, no continua la función.
         return;
     }
-    event.preventDefault();
+
+    event.preventDefault(); // Evita que se redireccione a la página con un query string
     alert('Correo enviado');
-    window.location.href = 'index.html';
+    window.location.href = 'index.html'; // Redirije a la página inicial, lo que se haría por defecto pero una query string
 });
 
 // Este archivo Javascript no se "minificó" por su tamaño reducido.
